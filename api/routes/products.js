583b8c9892df47ff2bdf249c3ab0bf0f8ad955bf
Azad-Zed <aazad.zed@gmail.com>
2019-02-18 12:49:41 +0530

    Mongoose Validation and meaningfull response

--- api/routes/products.js
+++ api/routes/products.js
@@ -7,9 +7,24 @@ const Product = require('../models/product');
 router.get('/', (req, res, next) => {
     Product.find()
             .exec()
-            .then(docs =>{
-                console.log(docs);
-                res.status(200).json(docs);
+            .then(docs => {
+                // console.log(docs);
+                // res.status(200).json(docs);
+                const response = {
+                    count: docs.length,
+                    products: docs.map(doc =>{
+                        return {
+                            name: doc.name,
+                            price: doc.price,
+                            _id: doc._id,
+                            request:{
+                                type: 'GET',
+                                url: 'http://localhost:3000/products/' + doc.id
+                            }
+                        }
+                    })
+                }
+                res.status(200).json(response);
             })
             .catch(err =>{
                 console.log(err);
@@ -17,6 +32,7 @@ router.get('/', (req, res, next) => {
                     error: err
                 });
             });
+            
 });
 
 
@@ -31,10 +47,18 @@ router.post('/', (req, res, next) => {
     product
         .save()
         .then(result => {
-            console.log(result);
+            // console.log(result);
             res.status(200).json({
-                message: 'Handling POST requests to /products',
-                createdProduct: result
+                message: 'Product saved successfully.',
+                createdProduct: {
+                    name: result.name,
+                    price: result.price,
+                    _id: result._id,
+                    request:{
+                        type: 'GET',
+                        url: 'http://localhost:3000/products/' + result._id
+                    }
+                }
             });
         })
         .catch(err => {
@@ -46,11 +70,18 @@ router.post('/', (req, res, next) => {
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
     Product.findById(id)
+        .select('name price _id')
         .exec()
         .then(doc => {
             console.log("From database", doc);
             if(doc){
-                res.status(200).json(doc);
+                res.status(200).json({
+                    product: doc,
+                    request:{
+                        type: 'GET',
+                        url: 'http://localhost:3000/products'
+                    }
+                });
             }else{
                 res.status(404).json({message:"No valid entry found for this ID!"})
             }            
@@ -62,7 +93,7 @@ router.get('/:productId', (req, res, next) => {
 });
 
 router.patch('/:productId', (req, res, next) => {
-    const id = req.params.productId;
+const id = req.params.productId;
     const updateOps = {};
     for(const ops of req.body){
         updateOps[ops.propName] = ops.value;
@@ -72,7 +103,16 @@ router.patch('/:productId', (req, res, next) => {
             .exec()
             .then(result => {
                 console.log(result);
-                res.status(200).json(result);
+                res.status(200).json({
+                    message: 'Product updated successfully.',
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/products/' + id
+                    }
+                });
+                // res.status(200).json({
+                //     result
+                // });
             })
             .catch(err =>{
                 console.log(err);
@@ -88,7 +128,11 @@ router.delete('/:productId', (req, res, next) => {
     Product.remove({_id:id})
             .exec()
             .then(result =>{
-                res.status(200).json(result);
+                res.status(200).json({
+                    message: 'Product saved successfully.',
+                    deletedProduct: result
+                });
+                // res.status(200).json(result);
             })
             .catch(err => {
                 console.log(err);
